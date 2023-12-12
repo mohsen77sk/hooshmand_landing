e8e7cbbb134c664a28bf7a5c8a91388640b648f9
Mohsen77sk <Mohsen77sk@gmail.com>
2023-12-12 16:00:41 +0330

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
 import { OpenBankingComponent } from './modules/open-banking/open-banking.component';
 import { DataAnalysisComponent } from './modules/data-analysis/data-analysis.component';
+import { AssistancePaymentComponent } from './modules/assistance-payment/assistance-payment.component';
 import { NotFoundComponent } from './modules/not-found/not-found.component';
 
 export const routes: Routes = [
@@ -16,6 +17,10 @@ export const routes: Routes = [
   {
     path: 'data-analysis',
     component: DataAnalysisComponent,
+  },
+  {
+    path: 'assistance-payment',
+    component: AssistancePaymentComponent,
   },
   {
     path: 'not-found',
---
+++ src/app/modules/assistance-payment/assistance-payment.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { NavigationComponent } from '../../shared/navigation/navigation.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
+
+@Component({
+  selector: 'app-assistance-payment',
+  standalone: true,
+  imports: [CommonModule, NavigationComponent, FooterComponent],
+  templateUrl: './assistance-payment.component.html',
+  styleUrl: './assistance-payment.component.scss',
+  encapsulation: ViewEncapsulation.None,
+})
+export class AssistancePaymentComponent {}
